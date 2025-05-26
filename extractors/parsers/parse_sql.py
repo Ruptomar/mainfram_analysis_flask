72d8a735b78fda2b629460ec36dfaa82cab18bbd
simranjot270 <40738110+simranjot270@users.noreply.github.com>
2025-05-26 20:50:35 +0530

    Create parse_sql.py
    added parser for sql queries

---
+++ extractors/parsers/parse_sql.py
@@ -0,0 +1,17 @@
+import chardet
+import re
+
+def detect_encoding(cobol_code: bytes):
+    result = chardet.detect(cobol_code)
+    return result['encoding']
+
+def process_cobol_file(cobol_file):
+    cobol_code = cobol_file.read()
+    encoding = detect_encoding(cobol_code)
+    try:
+        decoded_code = cobol_code.decode(encoding)
+    except (UnicodeDecodeError, TypeError):
+        decoded_code = cobol_code.decode('utf-8', errors='ignore')
+    sql_queries = re.findall(r'EXEC SQL.*?END-EXEC', decoded_code, re.DOTALL)
+    print("sql_queries analysis completed")
+    return sql_queries
